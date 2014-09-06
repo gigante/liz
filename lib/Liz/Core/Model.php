c22ccb5539cfdb3a3d78a21818361ae7d2773ff5
Hiarison Gigante <gigantedesousa@gmail.com>
2014-09-06 17:04:06 -0300

    Add support for pgsql in Core Model

--- lib/Liz/Core/Model.php
+++ lib/Liz/Core/Model.php
@@ -35,18 +35,25 @@ class Model
 	public function newConnection($dsn = null)
 	{
 		if($dsn == null) {
+			$db     = $this->config->getItem($this->dsn)['db'];
 			$user   = $this->config->getItem($this->dsn)['db.username'];
 			$pass   = $this->config->getItem($this->dsn)['db.password'];
 			$host   = $this->config->getItem($this->dsn)['db.host'];
 			$dbname = $this->config->getItem($this->dsn)['db.dbname'];
 			$port   = $this->config->getItem($this->dsn)['db.port'];	
 		} else {
+			$db     = $this->config->getItem($dsn)['db'];
 			$user   = $this->config->getItem($dsn)['db.username'];
 			$pass   = $this->config->getItem($dsn)['db.password'];
 			$host   = $this->config->getItem($dsn)['db.host'];
 			$dbname = $this->config->getItem($dsn)['db.dbname'];
 			$port   = $this->config->getItem($dsn)['db.port'];
 		}		
-		return new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $pass);
+
+		if($db == 'mysql') {
+			return new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $pass);	
+		} else if($db == 'pgsql') {
+			return new PDO("pgsql:host={$host};port={$port};dbname={$dbname};user={$user};password={$pass}");
+		}
 	}
 }
